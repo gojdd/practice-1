4ec5f1feebfe8ebd14aaf37ce940f8ec935cc6d9
gojdd <tptprlsea@gmail.com>
2017-07-24 00:02:56 +0900

    上手くいかない

--- 13.0.rb
+++
@@ -1,8 +0,0 @@
-score=gets.to_i
-if score>90 
-puts "very great!"
-elsif score>60
-puts "soso...."
-else
-puts "very bad!"
-end
--- 6.0.rb
+++
@@ -1,8 +0,0 @@
-puts "price #{1000*3}"
-puts 'price #{1000*3}'
-
-puts "hello\n world\t"
-puts 'hello\n world\t'
-
-puts "hello"+"world"
-puts "hello\n"*3
